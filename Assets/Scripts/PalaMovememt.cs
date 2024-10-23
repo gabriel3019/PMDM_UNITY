5ade658a9d7fa30084d6f1dafdcc09168f24ea90
gabriel3019 <gabrielbr190@gmail.com>
2024-10-23 14:14:21 +0200

    Inizital commit
    Unity

---
+++ Assets/Scripts/PalaMovememt.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalaMovememt : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private float x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(x, y) * speed;
+    }
+}
